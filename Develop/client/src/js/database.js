5031dfb02161748670787dd37f1ed083a95cc495
itlleat <scott.schulman84@gmail.com>
2023-05-09 20:22:25 -0600

    database and install

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log("item added to the database", result);  
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("all items from the database have been retrieved", result);
+  return result?.[0]?.value;  
+};
 
 initdb();
